fe3f7ea761e054acdd1b4cbda38e30bb4a35f0c8
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-03 17:35:17 +0300

    Получение данных из другого приложения (ShoppingList)

--- app/src/main/java/ru/potemkin/contentprovidertest/MainActivity.kt
+++ app/src/main/java/ru/potemkin/contentprovidertest/MainActivity.kt
@@ -1,12 +1,32 @@
 package ru.potemkin.contentprovidertest
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        thread {
+            val cursor = contentResolver.query(
+                Uri.parse("content://com.potemkin.shoppinglist/shop_items"),
+                null, null, null, null, null
+            )
+            while (cursor?.moveToNext() == true) {
+                val id = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
+                val name = cursor.getString(cursor.getColumnIndexOrThrow("name"))
+                val count = cursor.getInt(cursor.getColumnIndexOrThrow("count"))
+                val enabled = cursor.getInt(cursor.getColumnIndexOrThrow("enabled")) > 0
+                val shopItem = ShopItem(
+                    name, count, enabled, id
+                )
+                Log.d("MainActivity", shopItem.toString())
 
+            }
+            cursor?.close()
+        }
     }
 }
